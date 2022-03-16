e371c32ae4e584a670090561deefe22d2fa7fb80
AliAlkadhim <aa18dg@my.fsu.edu>
2022-03-16 16:43:34 -0400

    very rough start

--- .virtual_documents/Start.ipynb.py
+++ .virtual_documents/Start.ipynb.py
@@ -1,5 +1,9 @@
 import numpy as np
+import scipy as sp
 import scipy.stats as st
+
+
+
 
 
 Bprime=10000
@@ -12,9 +16,6 @@ print('The observed luminosity: ', L_obs)
 # print('The observed background'
 
 
-
-
-
 def lambd(D, theta, thetahat=1):#test statistic
     L_num = st.norm.pdf(D, loc= theta, scale=1)#the gaussian pdf of D counts
     return L_num
@@ -23,7 +24,7 @@ def lambd(D, theta, thetahat=1):#test statistic
 #T=[[theta_i],[Z_i]]
 T = [[],[]]
 for i in range(Bprime):
-    theta = np.random.poisson(500)#draw a count theta from a radom poisson prior, it has to be count because its an input to a poisson
+    theta = round(np.random.normal(10))#draw a count theta from a radom poisson prior, it has to be count because its an input to a poisson. This prior should also be close to the cound D
     X_mean = np.random.poisson(lam=theta) #draw count samples randomly from a poisson distribution
     lam_true = lambd(D, theta)
     lam_i = lambd(X_mean, theta)
@@ -35,10 +36,21 @@ for i in range(Bprime):
     T[1].append(Z_i)
 
 
-np.array(T[1]).sum()
+def E_hat(T):
+    """The expectation value of Z as a relative frequency, this should equal p_hat, the learned parameterized distribution at a given theta"""
+    num = np.array(T[1]).sum()
+    den = Bprime
+    return num/den
 
 
-np.random.poisson(lam=500)
+E_hat(T)
+
+
+def p_calculated(theta):
+    return sp.special.gammainc(D, theta)
+
+
+p = p_calculated(theta = round(np.random.normal(10))); p
 
 
 np.random.gamma(5)
